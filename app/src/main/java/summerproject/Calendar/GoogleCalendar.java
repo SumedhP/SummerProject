fde80192ba92151cce76d62dbcd7990cfadf0925
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-06-22 17:31:29 -0700

    Moved calender into seperate class

--- app/src/main/java/summerproject/Calendar/GoogleCalendar.java
+++ app/src/main/java/summerproject/Calendar/GoogleCalendar.java
@@ -1,24 +1,52 @@
 package summerproject.Calendar;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.Event;
 
+import summerproject.CalendarQuickstart;
+
 public class GoogleCalendar implements summerproject.Calendar.Calendar{
-    
+
+    private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
+
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
+
+    /** Directory to store authorization tokens for this application. */
+	private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+	/**
+	 * Global instance of the scopes required by this quickstart.
+	 * If modifying these scopes, delete your previously saved tokens/ folder.
+	 */
+	private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR_READONLY);
+	private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+    
     
     private final NetHttpTransport HTTP_TRANSPORT;
     
     private final Calendar calendar;
 
-    public GoogleCalendar(){
+    public GoogleCalendar() throws GeneralSecurityException, IOException{
         HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         calendar = new Calendar.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
@@ -27,25 +55,42 @@ public class GoogleCalendar implements summerproject.Calendar.Calendar{
 
     @Override
     public List<Event> getEvents() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public List<Event> getEvents(Date startTime) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public boolean addEvent(Event event) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean deleteEvent(String eventId) {
-        // TODO Auto-generated method stub
         return false;
     }
+
+	private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+		// Load client secrets.
+		InputStream in = CalendarQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+		if (in == null) {
+			throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+		}
+		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+		// Build flow and trigger user authorization request.
+		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+				HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+				.setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+				.setAccessType("offline")
+				.build();
+                
+		LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+		Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+		//returns an authorized Credential object.
+		return credential;
+	}
 }
