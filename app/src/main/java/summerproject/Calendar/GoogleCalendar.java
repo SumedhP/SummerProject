5e2840a70351c1680ba7bb861a923c53fe6fbfe3
SumedhP <43325361+SumedhP@users.noreply.github.com>
2022-06-23 17:11:55 -0700

    Made import nicer

--- app/src/main/java/summerproject/Calendar/GoogleCalendar.java
+++ app/src/main/java/summerproject/Calendar/GoogleCalendar.java
@@ -1,19 +1,13 @@
 package summerproject.Calendar;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.*;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
--- app/src/main/java/summerproject/CalendarQuickstart.java
+++ app/src/main/java/summerproject/CalendarQuickstart.java
@@ -1,30 +1,13 @@
 package summerproject;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
+
 import com.google.api.client.util.DateTime;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.calendar.Calendar;
-import com.google.api.services.calendar.CalendarScopes;
-import com.google.api.services.calendar.model.Event;
-import com.google.api.services.calendar.model.Events;
+import com.google.api.services.calendar.model.*;
 
 import summerproject.Calendar.GoogleCalendar;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Collections;
-import java.util.List;
 
 /* class to demonstarte use of Calendar events list API */
 public class CalendarQuickstart {
@@ -42,7 +25,9 @@ public class CalendarQuickstart {
 				.setOrderBy("startTime")
 				.setSingleEvents(true)
 				.execute();
+
 		List<Event> items = events.getItems();
+
 		if (items.isEmpty()) {
 			System.out.println("No upcoming events found.");
 		} else {
@@ -55,5 +40,6 @@ public class CalendarQuickstart {
 				System.out.printf("%s (%s)\n", event.getSummary(), start);
 			}
 		}
+
 	}
 }
